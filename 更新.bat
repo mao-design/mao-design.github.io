4c70e9477b32b8a1d7379e2e44035b554d30be48
mao-design <75081751+mao-design@users.noreply.github.com>
2024-03-05 07:03:48 +0000

    github action Updated By Github Actions

---
+++ 更新.bat
@@ -0,0 +1,11 @@
+git add *
+
+git commit -m "github action"
+
+git remote add origin git@github.com:mao-design/gitee_pull.git
+
+git branch -M main
+
+git pull origin main
+
+git push -u origin main
